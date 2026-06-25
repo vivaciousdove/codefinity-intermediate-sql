d6f60d569b0ed6cf88fcf3a3b71b631f538c0409
vivaciousdove <>
2026-06-25 21:50:16 +0000

    Cheat detection

---
+++ grouping/cheat_detection/query.sql
@@ -0,0 +1,23 @@
+/*SELECT department
+FROM employees
+GROUP BY department
+HAVING AVG(salary) < 70000*/
+
+/*SELECT student_surname
+from student_grades
+GROUP BY student_surname
+HAVING COUNT(class_letter) >1
+order by student_surname
+*/
+
+/*select student_surname, avg(grade) as average_grade
+from student_grades
+group by student_surname
+having count(grade) >1
+order by student_surname*/
+
+select student_surname
+from student_grades
+where subject_name = 'Mathematics'
+group by student_surname
+having count(grade) >1
